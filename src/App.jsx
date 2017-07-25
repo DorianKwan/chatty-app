4f1e2a7533742a64d2c835b60ac8ad40d2996a66
Bryce <bryce.sayers-kwan@live.ca>
2017-07-25 20:45:55 +0000

    Missed added changes to App.jsx in last commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import NavBar from './NavBar.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
+function uniqueId() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = { 
-      currentUser: {name: 'Bob'}, 
+      currentUser: { name: 'Bob' }, 
       messages: [
         {
+          id: uniqueId(),
           username: 'Bob',
           content: 'Has anyone seen my marbles?',
         },
         {
+          id: uniqueId(),
           username: 'Anonymous',
           content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
         }
@@ -25,13 +31,15 @@ class App extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.setState({ 
-      currentUser: {name: 'Bob'}, 
+        currentUser: { name: 'Bob' }, 
         messages: [
           {
+            id: uniqueId(),
             username: 'Bob',
             content: 'Has anyone seen my marbles?',
           },
           {
+            id: uniqueId(),
             username: 'Anonymous',
             content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
           }
