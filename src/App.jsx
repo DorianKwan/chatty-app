e7c7692f15c46f92752aca48b73d8d12f75e056f
Bryce <bryce.sayers-kwan@live.ca>
2017-07-25 19:58:55 +0000

    Added intial state to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,42 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentUser: {name: 'Bob'}, 
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ 
+      currentUser: {name: 'Bob'}, 
+        messages: [
+          {
+            username: 'Bob',
+            content: 'Has anyone seen my marbles?',
+          },
+          {
+            username: 'Anonymous',
+            content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+          }
+        ]
+      })
+    }, 500)
+  }
+
   render() {
     return (
       <div id="react-root">
